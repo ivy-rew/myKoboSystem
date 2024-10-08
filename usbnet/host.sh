2ef26e945bfcfb6d64f99821d622c1686a9fdb0b
LordEagle <r.wermelinger@gmail.com>
2024-10-08 21:07:11 +0200

    generalize setting usb-host ip addr

--- usbnet/host.sh
+++ usbnet/host.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-usbdev=$(ip link | grep 'enx[^ ]' | awk '{print $2}' | tr -d ':')
+usbdev="usb0"
+if [[ -z $(ip link | grep "usb0" ) ]]; then
+  usbdev=$(ip link | grep 'enx[^ ]' | awk '{print $2}' | tr -d ':')
+fi
 echo $usbdev
 
-# only ecdas works to connect!
+usbIP(){
+  sudo ip addr add 192.168.2.1 broadcast + dev $usbdev
+  sudo ip route add 192.168.2.0/24 dev $usbdev
+}
+
+# enable SSH server in kordeader; with "passwordless" auth
+# 
+# only ecdsa works to connect: so generate one just for kobo
 # ssh-keygen -t ecdsa 
+# read its public key cat ~/.ssh/*.pub
+# add it manually to: /mnt/onboard/.adds/koreader/settings/SSH/authorized_keys
+# then remove SSH server open 'passwordless'
+
 
 # https://www.mobileread.com/forums/showthread.php?t=353810
 
