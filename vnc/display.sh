c5e25b999f5abbfd2b8822bc9d36a8594a480a79
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-10-22 16:17:09 +0200

    automate virtual e-ink display creation

--- vnc/display.sh
+++ vnc/display.sh
@@ -2,32 +2,23 @@
 
 # https://superuser.com/questions/1086133/vnc-server-configuration-for-multi-monitor-support
 
-# H20
-#w=1680
-#h=1264
+. ./env.sh
 
-#Eclipsa2E
-w=1404
-h=1872
 fps=30
 
 newMonitor(){
-  modeline=$(gtf $w $h $fps)
-  echo $modeline
+  gtf $w $h $fps | grep -o -E '".*'
 }
 
-enable(){
-  virtual="HDMI-2"
-  virtual="HDMI-A-0"
-  #xrandr --newmode "$wx$h_$fps.00"  83.33  1680 1744 1920 2160  1264 1265 1268 1286  -HSync +Vsync
+einkOutput(){
   mode="${w}x${h}_${fps}.00"
-  xrandr --newmode "1408x1872_30.00"  106.93  1408 1488 1640 1872  1872 1873 1876 1904  -HSync +Vsync
-  xrandr --addmode $virtual "${mode}"
-  xrandr --output $virtual --mode "${mode}" --right-of eDP
+  xrandr --newmode $(newMonitor)
+  echo "adding mode"
+  xrandr --addmode $virtual "\"${mode}\""
+  echo "output to ${virtual}"
+  xrandr --output $virtual --mode "\"${mode}\"" --right-of eDP
 }
 
 vncHost(){
-  # primary desk with
-  offset=1920
-  x11vnc -clip $wx$h+$offset+0
+  x11vnc -clip "${w}x${h}+${offset}+0"
 }
---
+++ vnc/env.template.sh
@@ -0,0 +1,12 @@
+# H20
+w=1680
+h=1260
+
+#Elipsa2E
+w=1404
+h=1872
+
+# display
+main=eDP
+virtual=HDMI-A-0
+offset=1920
